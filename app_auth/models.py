c41a14cfbd7a5b08727e158ea4942229ab8b3ce7
ninnroot <misterjames.thiha@gmail.com>
2023-06-23 17:38:16 +0800

    setup database

--- app_auth/models.py
+++ app_auth/models.py
@@ -12,16 +12,15 @@ class User(AbstractBaseUser, BaseModel, PermissionsMixin):
 
     role_choices = [
         ("admin",) * 2,
-        ("partnership-manager",) * 2,
-        ("agent-leader",) * 2,
-        ("agent-staff",) * 2,
-        ("customer",) * 2,
+        ("student",) * 2,
+        ("teacher",) * 2,
     ]
-    auth_method_choices = (("phone_number",) * 2, ("email",) * 2)
 
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    role = models.CharField(max_length=128, choices=role_choices)
 
     groups = None
     user_permissions = None
@@ -31,5 +30,6 @@ class User(AbstractBaseUser, BaseModel, PermissionsMixin):
 
     def __str__(self):
         return f"<User: {self.id} {self.email}>"
+
     class Meta:
         ordering = ("id",)
